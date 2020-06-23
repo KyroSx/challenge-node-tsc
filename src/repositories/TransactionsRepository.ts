c0bf7ab00c653d3652a5264d084e5c82e50ef432
leonardo <leolub150@gmail.com>
2020-06-23 09:21:12 -0300

    add Transaction Repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,49 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactionsFiltred = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const incomeBalance = incomeTransactionsFiltred.reduce(
+      (accum: number, transaction) => accum + transaction.value,
+      0,
+    );
+
+    const outcomeTransactionsFiltred = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const outcomeBalance = outcomeTransactionsFiltred.reduce(
+      (accum: number, transaction) => accum + transaction.value,
+      0,
+    );
+
+    const total = incomeBalance - outcomeBalance;
+
+    const balance = {
+      income: incomeBalance,
+      outcome: outcomeBalance,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      type,
+      value,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
